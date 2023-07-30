2b7f4ea681ad77993d3bc45721c738ea08eb93ba
Shajenthika <113460347+Shajenthika@users.noreply.github.com>
2023-07-30 14:19:55 +0530

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,27 @@
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <a class="navbar-brand" href="index.php"><img src="logo.jpeg" alt="" width="25px" ></a>
+                    <a class="navbar-brand" href="index.php">ABC Website</a>
+                </div>
+                <div class="col-md-6">
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#customer">Customer</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#item">Items</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#report">Reports</a>
+                            </li>
+                        </ul>
+                    </div>    
+                </div>
+            </div>
+            
+        </div>
+    </nav>
