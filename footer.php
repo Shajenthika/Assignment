1ab1e0de6d37afec8fe831e274e5f09477658583
Shajenthika <113460347+Shajenthika@users.noreply.github.com>
2023-07-30 14:20:10 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,13 @@
-
+    <!-- Bootstrap JS script -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   </body>
+    <footer class="footer bg-dark text-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>&copy; ABC Company. All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
 </html>
