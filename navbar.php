7881ca99ff11cefb76ec78a1c3eb7d38ea37a3a2
Shajenthika <113460347+Shajenthika@users.noreply.github.com>
2023-07-30 16:12:31 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,27 +1,25 @@
     <!-- Navigation Bar -->
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
         <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <a class="navbar-brand" href="index.php"><img src="logo.jpeg" alt="" width="25px" ></a>
-                    <a class="navbar-brand" href="index.php">ABC Website</a>
-                </div>
-                <div class="col-md-6">
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#customer">Customer</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#item">Items</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#report">Reports</a>
-                            </li>
-                        </ul>
-                    </div>    
-                </div>
-            </div>
+            <a class="navbar-brand" href="index.php"><img src="logo.jpeg" alt="" width="25px" ></a>
+            <a class="navbar-brand" href="index.php">ABC Website</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
             
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#customer">Customer</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#item">Items</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#report">Reports</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
